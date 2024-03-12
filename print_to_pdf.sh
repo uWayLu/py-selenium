51a0d6299e8efbef4c00b061b807d3dc6ba9648a
uWayLu <u.master.o.twn@gmail.com>
2024-03-12 16:33:19 +0800

    fix: add option for root user

--- print_to_pdf.sh
+++ print_to_pdf.sh
@@ -2,5 +2,5 @@
 workdir=$(dirname $(readlink -f $0))
 cd $workdir
 . ./.venv/bin/activate
-chrome --headless --print-to-pdf="$2" "$1" "${@:3}"
+chrome --headless --sandobx --print-to-pdf="$2" "$1" "${@:3}"
 
