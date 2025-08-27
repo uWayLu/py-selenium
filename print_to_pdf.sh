fa04c0a48d7b91c7887b122a30fb60d1af3f6031
uWayLu <u.master.o.twn@gmail.com>
2025-08-27 12:14:55 +0800

    feat: print to pdf ignore insecure ssl

--- print_to_pdf.sh
+++ print_to_pdf.sh
@@ -3,5 +3,7 @@ workdir=$(dirname $(readlink -f $0))
 cd $workdir
 . ./.venv/bin/activate
 chrome --headless --no-sandbox --print-to-pdf="$2" "$1" "${@:3}" \
-	--no-pdf-header-footer \
+    --no-pdf-header-footer \
+    --ignore-certificate-errors
+
 
